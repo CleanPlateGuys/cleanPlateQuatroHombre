c314772b0e900f2d6dcb3037d1e26b63f85afd85
carlos.chavira333 <carlos.chavira333@gmail.com>
2022-07-12 15:08:32 -0500

    initial commit

---
+++ src/main/java/com/example/cleanplatequatrohombres/CleanPlateQuatroHombresApplication.java
@@ -0,0 +1,13 @@
+package com.example.cleanplatequatrohombres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanPlateQuatroHombresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanPlateQuatroHombresApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/cleanplatequatrohombres/controllers/HomeController.java
@@ -0,0 +1,7 @@
+package com.example.cleanplatequatrohombres.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+}
